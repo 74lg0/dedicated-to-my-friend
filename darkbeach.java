656452fcc69c8cdb9794db4ab9ff61dd9077f7a2
74lg0 <111157836+74lg0@users.noreply.github.com>
2026-03-17 17:59:09 -0600

    Add darkbeach.java to display lyrics
    This Java program displays a series of lyrics in a dialog box using JOptionPane.

---
+++ darkbeach.java
@@ -0,0 +1,41 @@
+/* 
+✧･ﾟ: *✧･ﾟ:*  Code by 74lg0  *:･ﾟ✧*:･ﾟ✧
+            ⟡  to  ⟡
+    ♡ Lilith, my best friend <3 ♡
+✧･ﾟ: *✧･ﾟ:*  ∞ ✧ ∞ ✧ ∞  *:･ﾟ✧*:･ﾟ✧
+*/
+import javax.swing.*;
+public class darkbeach {
+    public static void main(String[] args) {
+        // Creamos la clase Frame
+        JFrame f = new JFrame();
+        String[] lyric = {
+            "Laying on a dark beach",
+            "Pulsing waves cover me",
+            "Flowers bloom on the sea",
+            "Silver sand underneath",
+            "Talk to me as I am sleeping",
+            "Hold me while I'm dreaming",
+            "Honestly, I could just breathe you in",
+            "Met you when my heart was bleeding",
+            "I'm constantly feeling",
+            "Drifting through an endless reverie",
+            "Twilight moon, interlude",
+            "Fell in love in my youth",
+            "On a cloud, not a sound",
+            "Silent kiss just like this",
+            "Talk to me as I am sleeping",
+            "Hold me while I'm dreaming",
+            "Honestly, I could just breathe you in",
+            "Met you when my heart was bleeding",
+            "I'm constantly feeling",
+            "Drifting through an endless reverie"
+        };
+        
+        // Bucle for para recorrer el array
+        for (String lyric1 : lyric) {
+            JOptionPane.showMessageDialog(f, lyric1);
+        }
+    }
+    
+}
