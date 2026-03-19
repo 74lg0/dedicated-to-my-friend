f0a288b753a1316e67c36292970695f592b9d6d6
74lg0 <111157836+74lg0@users.noreply.github.com>
2026-03-19 00:12:23 -0600

    Refactor lyrics and enhance dialog title

--- darkbeach.java
+++ darkbeach.java
@@ -1,41 +1,89 @@
-/* 
+/*
 ✧･ﾟ: *✧･ﾟ:*  Code by 74lg0  *:･ﾟ✧*:･ﾟ✧
             ⟡  to  ⟡
     ♡ Lilith, my best friend <3 ♡
 ✧･ﾟ: *✧･ﾟ:*  ∞ ✧ ∞ ✧ ∞  *:･ﾟ✧*:･ﾟ✧
 */
+
 import javax.swing.*;
 public class darkbeach {
     public static void main(String[] args) {
         // Creamos la clase Frame
         JFrame f = new JFrame();
         String[] lyric = {
-            "Laying on a dark beach",
-            "Pulsing waves cover me",
-            "Flowers bloom on the sea",
-            "Silver sand underneath",
-            "Talk to me as I am sleeping",
-            "Hold me while I'm dreaming",
-            "Honestly, I could just breathe you in",
-            "Met you when my heart was bleeding",
-            "I'm constantly feeling",
-            "Drifting through an endless reverie",
-            "Twilight moon, interlude",
-            "Fell in love in my youth",
-            "On a cloud, not a sound",
-            "Silent kiss just like this",
-            "Talk to me as I am sleeping",
-            "Hold me while I'm dreaming",
-            "Honestly, I could just breathe you in",
-            "Met you when my heart was bleeding",
-            "I'm constantly feeling",
-            "Drifting through an endless reverie"
+                "Laying on",
+                "a dark beach",
+                "Pulsing waves",
+                "cover me",
+                "Flowers bloom",
+                "on the sea",
+                "Silver sand",
+                "underneath",
+                "Talk to me",
+                "as I am sleeping",
+                "Hold me while",
+                "I'm dreaming",
+                "Honestly",
+                "I",
+                "could",
+                "just",
+                "breathe",
+                "you",
+                "in",
+                "Met",
+                "you",
+                "when",
+                "my",
+                "heart",
+                "was",
+                "bleeding",
+                "I'm",
+                "constantly",
+                "feeling",
+                "Drifting",
+                "through",
+                "an endless",
+                "reverie",
+
+                // Aquí termina la seccion rapida
+                "Twilight moon, interlude",
+                "Fell in love in my youth",
+                "On a cloud, not a sound",
+                "Silent kiss just like this",
+
+                // Iniciamos de nuevo con la seccion rapida
+                "Talk to me",
+                "as I am sleeping",
+                "Hold me while",
+                "I'm dreaming",
+                "Honestly",
+                "I",
+                "could",
+                "just",
+                "breathe",
+                "you",
+                "in",
+                "Met",
+                "you",
+                "when",
+                "my",
+                "heart",
+                "was",
+                "bleeding",
+                "I'm",
+                "constantly",
+                "feeling",
+                "Drifting",
+                "through",
+                "an endless",
+                "reverie",
+                "<3333333333 TQM"
         };
-        
+
         // Bucle for para recorrer el array
         for (String lyric1 : lyric) {
-            JOptionPane.showMessageDialog(f, lyric1);
+            JOptionPane.showMessageDialog(f, lyric1, "DarkBeach - Pastel Ghost", JOptionPane.INFORMATION_MESSAGE);
         }
     }
-    
+
 }
